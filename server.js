52b0f79f7504d92a7d9bbc7a9e567ac350c81407
Chris Chapman <Chapman@apextion.com>
2018-01-08 11:54:20 -0500

    added simple express server (again)

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Working hard or hardly working?');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', (req, res) => {
+  console.log('route activated');
+  res.send('DERP!');
+});
+
+exports.server = app.listen(port, () => {
+  console.log('Server Active On Port', port);
+});
